1f70cf664afe8c5df0630efb9f2822e0ff6320e9
Alejandro Biarrieta <103573136+Reinapepeada@users.noreply.github.com>
2024-12-03 17:31:48 -0300

    Update comparacion.py

--- comparacion.py
+++ comparacion.py
@@ -24,6 +24,8 @@ def compare_plans(plan1, plan2):
     optative2 = count_optatives(subjects2)
     different = {}
     missing = {}
+    missing_from_second = {}
+    subjects_left = subjects1.copy() - subjects2.copy()
 
     for subject in subjects1:
         if "OPTATIVA" in subject:
@@ -37,20 +39,25 @@ def compare_plans(plan1, plan2):
         if "OPTATIVA" in subject:
             continue
         if subject not in subjects1:
-            missing[subject] = 1
+            different[subject] = 1
+            missing_from_second[subject] = 1
 
-    return included, different, missing, optative1, optative2
+    
+    
 
+    return included, different, missing, optative1, optative2, missing_from_second, subjects_left
 
 # Load the plans
 plan1 = load_plan('./planes/LICENCIATURA_EN_GESTIÓN_DE_TECNOLOGÍA_DE_LA_INFORMACIÓN.json')
 plan2 = load_plan('./planes/TECNICATURA_UNIVERSITARIA_EN_DESARROLLO_DE_SOFTWARE.json')
 
 # Compare the plans
-included, different, missing, optative1, optative2= compare_plans(plan1, plan2)
+included, different, missing, optative1, optative2, missing_from_second, subjects_left = compare_plans(plan1, plan2)
 
-print("Included in both plans:", included, "\nCount:", len(included), "\n")
-print("Different between plans:", different, "\nCount:", len(different), "\n")
-print("Missing in the first plan:", missing, "\nCount:", len(missing), "\n")
-print("Optative subjects in the first plan:", optative1, "\nCount:", optative1, "\n")
-print("Optative subjects in the second plan:", optative2, "\nCount:", optative2, "\n")
+# Print the results
+print("Subjects approved in the previous plan (excluding optatives):", len(included))
+print("Subjects different in the second plan (missing from the first plan):", len(different))
+print("Total optative subjects in both plans:", optative1 + optative2)
+print("Subjects approved in the previous plan:", len(included) + optative1)
+print("Subjects missing from the second plan to complete it:", len(missing_from_second))
+print("Subjects left in the second plan:", len(subjects_left))
